2affeed823308a94258fbf486be30f732767b0b0
ATS1\Francis.Yanga <francis.yanga@ruralco.co.nz>
2025-06-26 13:19:26 +1200

    added bootstrap, pages and routes

---
+++ src/app/Pages/add-job-application/add-job-application.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-job-application',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-job-application.component.html',
+  styleUrls: ['./add-job-application.component.scss']
+})
+export class AddJobApplicationComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { AddJobApplicationComponent } from './Pages/add-job-application/add-job-application.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'add-application',
+    component: AddJobApplicationComponent
+  }
+];
